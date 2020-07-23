33cd0cd76809110e90cc8f93a13d7e24e542e385
Huy TA QUOC <htquoc@linagora.com>
2020-07-23 10:47:07 +0700

    Added StencilJS default setup for web components

---
+++ src/utils/fetch-icon.ts
@@ -0,0 +1,14 @@
+export default function fetchSvgIcon(iconUrl: string) {
+  console.log('fetching from iconUrl', iconUrl);
+
+  const proxiedUrl = 'https://cors-anywhere.herokuapp.com/' + iconUrl;
+
+  console.log('proxiedUrl', proxiedUrl);
+
+  return fetch(proxiedUrl)
+    .then((response) => response.text())
+    .then((svgContent) => {
+      console.log('svgContent', svgContent);
+      return svgContent;
+    });
+}
