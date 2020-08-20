d25121c1317fbea1088e2dcb98dc496ecc137c54
Huy TA QUOC <htquoc@linagora.com>
2020-08-20 14:45:52 +0700

    https://github.com/OpenPaaS-Suite/esn-frontend-application-grid/issues/9: Now will rerender when the 'serializedApplications' prop/attribute changes

--- src/components/esn-app-grid/esn-app-grid.tsx
+++ src/components/esn-app-grid/esn-app-grid.tsx
@@ -1,4 +1,4 @@
-import { Component, Prop, ComponentInterface, State, Host, h } from '@stencil/core';
+import { Component, Prop, ComponentInterface, Host, h } from '@stencil/core';
 import { Application } from './esn-app-grid.types';
 import { AppGridTogglerIcon } from '../../icons/AppGridIcon';
 import { getAppIcon } from '../../utils/app-icons';
@@ -12,33 +12,38 @@ import { isParentPathnameOf } from '../../utils/pathname';
 export class EsnAppGrid implements ComponentInterface {
   private popover: HTMLElement;
 
-  @State() applications: Application[] = [];
-
   /**
    * A serialized JSON object that contains information about the available apps.
    */
   @Prop() serializedApplications: string;
 
-  componentWillLoad() {
+  parseSerializedApplications() {
+    let applications: Application[];
+
     try {
       if (!this.serializedApplications) {
         throw new Error("The 'serializedApplications' attribute/property must be set to a serialized JSON");
       }
 
-      const applications = JSON.parse(this.serializedApplications);
+      applications = JSON.parse(this.serializedApplications);
 
       if (!Array.isArray(applications) || !applications.length) {
         throw new Error("The parsed 'serializedApplications' has an invalid JSON structure");
       }
-
-      this.applications = applications;
     } catch (err) {
-      console.error('Something went wrong', err);
-      this.applications = [];
+      console.error("Something went wrong when parsing the 'serializedApplications' property.", err);
+
+      if (!Array.isArray(applications) || applications.length) {
+        applications = [];
+      }
     }
+
+    return applications;
   }
 
   render() {
+    const applications = this.parseSerializedApplications();
+
     return (
       <Host>
         <div class="esn-app-grid">
@@ -53,7 +58,7 @@ export class EsnAppGrid implements ComponentInterface {
           </button>
           <esn-popover ref={el => (this.popover = el as HTMLElement)}>
             <div class="esn-app-grid__popover-content">
-              {this.applications.map(({ name, url }) => {
+              {applications.map(({ name, url }) => {
                 const IconComponent = getAppIcon(name);
 
                 return (
